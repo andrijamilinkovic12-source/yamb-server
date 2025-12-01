496a574987a57cad149eace46363a5ae3c4502f0
Andrija <andrija.milinkovic12@gmail.com>
2025-12-01 23:40:29 +0100

    Popravka: Izbacen node_modules i ubacen public

--- public/js/main.js
+++
@@ -1,7 +0,0 @@
-import { YambApp } from './YambApp.js';
-
-// Odmah pravimo instancu i kačimo je na window
-// Moduli su po prirodi "deferred" (odloženi), tako da će HTML već biti spreman
-window.app = new YambApp();
-
-console.log("Yamb aplikacija inicijalizovana!"); // Ovo služi da vidimo u konzoli da li radi
--- public/js/utils.js
+++
@@ -1,2 +0,0 @@
-export const sleep = ms => new Promise(r => setTimeout(r, ms));
-export const sum = arr => arr.reduce((a, b) => a + b, 0);
