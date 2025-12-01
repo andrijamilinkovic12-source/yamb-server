f2f011221486ab3526c68b7299bea5488ed71faa
Andrija <andrija.milinkovic12@gmail.com>
2025-12-01 23:32:37 +0100

    Potpuni reset fajlova

--- public/js/constants.js
+++
@@ -1,4 +0,0 @@
-export const DICE_SYMBOLS = {0: "?", 1: "⚀", 2: "⚁", 3: "⚂", 4: "⚃", 5: "⚄", 6: "⚅"};
-export const REDOVI_IGRA = ["1", "2", "3", "4", "5", "6", "Max", "Min", "Triling", "Kenta", "Ful", "Poker", "Yamb"];
-export const REDOVI_PRIKAZ = ["1", "2", "3", "4", "5", "6", "ZBIR 1", "Max", "Min", "ZBIR 2", "Triling", "Kenta", "Ful", "Poker", "Yamb", "ZBIR 3"];
-export const KOLONE = ["Nadole", "Slobodna", "Sredina", "Nagore", "Ručno", "Najava"];
